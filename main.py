aa29a7005dd9119617b16f10af1845ca5c1a4e56
Dola Rani Mondal <addylin.dola@gmail.com>
2025-12-31 19:33:32 +0600

    Add Supplier class and test in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from models.product import Product
 from models.perishable_product import PerishableProduct
-
+from models.supplier import Supplier
+from models.order import Order
 p1 = Product(1, "MacBook", 220000, 10)
 p1.add_stock(7)
 p1.reduce_stock(2)
@@ -8,3 +9,13 @@ print(p1)
 
 p2 = PerishableProduct(2, "Drinks", 80, 20, "2025-01-10")
 print(f"{p2.name} expired? {p2.is_expired('2025-01-11')}")
+
+
+s1 = Supplier(1, "ABC Traders", "0123456789")
+print(s1)
+
+
+order1 = Order(101, p1, 2)
+order1.process_order()
+print(order1)
+print(p1)  # Check updated stock
---
+++ models/supplier.py
@@ -0,0 +1,9 @@
+class Supplier:
+    def __init__(self, supplier_id, name, contact):
+        self.supplier_id = supplier_id
+        self.name = name
+        self.contact = contact
+
+    def __str__(self):
+        return f"{self.name} | Contact: {self.contact}"
+
